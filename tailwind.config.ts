da6d756d4c0e2c7874fe91bbff28d382f0eacf72
iazadur <iamazadur@gmail.com>
2024-09-25 07:35:15 +0600

    added chat page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,16 +3,17 @@ import colors from "tailwindcss/colors";
 import defaultTheme from "tailwindcss/defaultTheme";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        brand: '#E1E7EC',
-  			gray: colors.slate,
+  	extend: {
+  		colors: {
+  			brand: '#E1E7EC',
+  			gray: 'colors.slate',
   			'soft-gray': '#BABABA',
   			'cloud-gray': '#D8D8D8',
   			'cool-gray': '#E5E9EF',
@@ -23,18 +24,22 @@ const config: Config = {
   			midnight: '#282A36',
   			'midnight-100': '#525252',
   			'light-frost': '#E5E9EF'
-      },
-      borderRadius: {
-        '1.5': '6px',
-      },
-      boxShadow: {
-        'input': '0px 1px 2px 0px rgba(16, 24, 40, 0.05), 0px 0px 0px 4px #D4DBE5',
-      },
-      fontFamily: {
-        sans: ['"Aeonik"', ...defaultTheme.fontFamily.sans],
-      },
-    },
+  		},
+  		borderRadius: {
+  			'1.5': '6px',
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		boxShadow: {
+  			input: '0px 1px 2px 0px rgba(16, 24, 40, 0.05), 0px 0px 0px 4px #D4DBE5'
+  		},
+  		fontFamily: {
+  			sans: ['Aeonik', ...defaultTheme.fontFamily.sans]
+  		}
+  	}
   },
+    plugins: [require("tailwindcss-animate")]
 };
 
 export default config;
